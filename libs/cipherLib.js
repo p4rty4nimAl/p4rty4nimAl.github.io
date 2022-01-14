c21145c9e31cf8c4ffe2dad7c5bc6545505674ce
JLKingdoms <92207683+JLKingdoms@users.noreply.github.com>
2022-01-14 17:11:27 +0000

    rename ciphers, move common func to lib, finish cr cipher

---
+++ libs/cipherLib.js
@@ -0,0 +1,19 @@
+function reverseString(string) {
+  output = "";
+  for (i = 0; i < string.length; i++) {
+    output = string[i] + output;
+  }
+  return output;
+}
+function getVal(char) {
+  if (char != undefined) return char.toLowerCase().charCodeAt(0) - 96;
+}
+function getChar(val) {
+  return String.fromCharCode((val % 32) + 96);
+}
+function encipher() {
+  document.getElementById("output").value = reverseString(bEF(reverseString(bEF(document.getElementById("input").value)))).replaceAll("`", " ").toLowerCase();
+}
+function decipher() {
+  document.getElementById("input").value = bDF(reverseString(bDF(reverseString(document.getElementById("output").value)))).replaceAll("`", " ").toLowerCase();
+}

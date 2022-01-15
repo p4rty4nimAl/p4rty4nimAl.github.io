48ea35285f7687e8fa322ebc20caf72e46e6c92b
JLKingdoms <92207683+JLKingdoms@users.noreply.github.com>
2022-01-15 21:44:26 +0000

    add missing semicolon

--- projects/continuousRegisterCipher/script.js
+++ projects/continuousRegisterCipher/script.js
@@ -12,7 +12,7 @@ function bEF(string) {
 function bDF(string) {
   output = string[0];
   for (i = 1; i < string.length; i++) {
-    output = output + getChar(getVal(string[i]) - getVal(string[i - 1]))
+    output = output + getChar(getVal(string[i]) - getVal(string[i - 1]));
   }
   return output;
 }
